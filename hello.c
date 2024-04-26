4d1e55bed280196fe56331fcaa1566ca9bc20b0a
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-26 18:31:13 +0000

    Sat, Apr 27, 2024, 12:01 AM +05:30

--- hello.c
+++ hello.c
@@ -1,5 +1,35 @@
+#include <cs50.h>
+
 #include <stdio.h>
 
-int main(void) {
-    printf("hello, world");
+#include <string.h>
+
+int main(void)
+
+{
+
+    string s = get_string("Before: ");
+
+    printf("After: ");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+
+    {
+
+        if (s[i] >= 'a' && s[i] <= 'z')
+
+        {
+
+            printf("%c", s[i] - 32);
+        }
+
+        else
+
+        {
+
+            printf("%c", s[i]);
+        }
+    }
+
+    printf("\n");
 }
