cfd90d795c5d4af8013de77cac0e4409a9403f62
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-27 16:46:08 +0000

    Sat, Apr 27, 2024, 10:16 PM +05:30

--- agree.c
+++ agree.c
@@ -1,8 +1,16 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
+int main(void)
+{
+    char c = get_char("Do you agree , y/n ");
 
-int main(void) {
-    char c = get_char("Do you agree , y/n")
-    
+        if (c == 'y')
+    {
+        printf("Agreed.\n");
+    }
+    else if (c == 'n')
+    {
+        printf("Not agreed.\n");
+    }
 }
