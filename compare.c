75a7cbf037c518591fcb4779cd0c32acb7c579f5
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-27 16:13:20 +0000

    Sat, Apr 27, 2024, 9:43 PM +05:30

---
+++ compare.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    int num = 58;
+    int guess = get_int("Guess a Number? ");
+    if (guess < num)
+    {
+        printf("Guess number is lesser than Orginal number \n");
+        guess = get_int("Guess a Number? ");
+    } else if(guess > num)
+    {
+        printf("Guess number is greater than Orginal number \n");
+        guess = get_int("Guess a Number? ");
+    } else
+    {
+        printf("Ta-dha %i\n", guess);
+    }
+    guess = get_int("Guess a Number? ");
+   return 0;
+}
