4f14411e515e3018280b50d2ae068c729b333dfe
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-27 16:50:39 +0000

    Sat, Apr 27, 2024, 10:20 PM +05:30

--- agree.c
+++ agree.c
@@ -5,11 +5,11 @@ int main(void)
 {
     char c = get_char("Do you agree , y/n ");
 
-        if (c == 'y')
+        if (c == 'y' || 'Y')
     {
         printf("Agreed.\n");
     }
-    else if (c == 'n')
+    else if (c == 'n' || 'N)
     {
         printf("Not agreed.\n");
     }
