b04ba0d33bf24c5245ac9ab90db9f8274ee9e418
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-29 21:49:48 +0000

    Tue, Apr 30, 2024, 3:19 AM +05:30

--- mario.c
+++ mario.c
@@ -3,13 +3,13 @@
 
 int main (void)
 {
+    int n;
     do
     {
-        while (n < 1)
-        {
-            int n = get_int("Size: ");
-        }
+        n = get_int("Size: ");
     }
+    while (n < 1);
+
     for(int i = 0; i < n; i++)
     {
         for(int j = 0; j < n; j++)
