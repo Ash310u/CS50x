9fdc7b763a6bb1cb87efd457b1a070afc47f1739
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-29 20:59:25 +0000

    Tue, Apr 30, 2024, 2:29 AM +05:30

--- calculator.c
+++ calculator.c
@@ -1,10 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void addition (int x, int y);
 int  main (void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
+    addition(x,y);
+}
 
-    printf("%i\n", x + y); 
+void addition (int x, int y)
+{
+    printf("%i\n", x + y);
 }
