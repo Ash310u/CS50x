527d8816d06bc1aad228c1910eda78af397874d8
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-29 21:36:15 +0000

    Tue, Apr 30, 2024, 3:06 AM +05:30

--- mario.c
+++ mario.c
@@ -4,13 +4,10 @@ int main (void)
 {
     for(int i = 0; i < 3; i++)
     {
-        char x;
         for(int j = 0; j < 3; j++)
         {
-            return printf("#");
+            printf("#");
         }
-        printf("%s\n", x);
-
-
+        printf("\n");
     }
 }
