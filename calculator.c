27a880bc4c3a957bce654c5c3115bee2c1ddd698
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-30 05:24:48 +0000

    Tue, Apr 30, 2024, 10:54 AM +05:30

--- calculator.c
+++ calculator.c
@@ -1,15 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int addition (int x, int y);
+// int addition (int x, int y);
 int  main (void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
-    printf("%i\n", addition(x,y));
+    // printf("%i\n", addition(x,y));
+
+    float z = (float) x / (float) y;
+    printf("%f\n", z);
 }
 
-int addition (int a, int b)
-{
-    return a + b;
-}
+// int addition (int a, int b)
+// {
+//     return a + b;
+// }
