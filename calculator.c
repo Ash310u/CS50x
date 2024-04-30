e06ac9c40aea91adb4ef1f6251b65c5ec3e1c82d
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-04-30 05:48:12 +0000

    Tue, Apr 30, 2024, 11:18 AM +05:30

--- calculator.c
+++ calculator.c
@@ -8,15 +8,21 @@ int  main (void)
     int y = get_int("y: ");
     // printf("%i\n", addition(x,y));
 
-    // (TYPE CASTING) treating one type as an another type
-    // If we dont use TYPE CASTING then the TRUNCATION happens(Truncation is a process or operation that shortens or cuts off a piece of data,
-    // such as a string or a file, by removing characters, digits, or bytes from the end.)
-    float z = (float) x / (float) y;
+    //////////////
+    // COMMENT ///////////////////////////////////////////////
+    /////////////////////////////////////////////////////////
+    // (TYPE CASTING) treating one type as an another type///////////////////////////////////////////////////////////////////////////////////////
+    // If we dont use TYPE CASTING then the TRUNCATION happens(Truncation is a process or operation that shortens or cuts off a piece of data,//
+    // such as a string or a file, by removing characters, digits, or bytes from the end //////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////
+
+    // float z = (float) x / (float) y;
     // "%.5f" it means show me 5 decimal places
-    printf("%.2f\n", z);
-    float z = (float) x / (float) y;
-    // "%.5f" it will work for Double also
-    printf("%.2f\n", z);
+    // printf("%.2f\n", z);
+
+    double z = (double) x / (double) y;
+    // "%.5f" this format specifiers will work for "Double" also
+    printf("%.20f\n", z);
 }
 
 // int addition (int a, int b)
