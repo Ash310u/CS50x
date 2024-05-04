b4a5142ebd8b607dd70122d06038ec45ecae7f2f
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-04 03:53:23 +0000

    Sat, May 4, 2024, 9:23 AM +05:30

--- Week-2/scores.c
+++ Week-2/scores.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main (void)
-{
-    int score[3] = [3, 5, 6]
-    // score[0] = 457;
-    // score[1] = 245;
-    // score[2] = 532;
+{               // jsut trying things.
+    int score[3]; // = [3, 5, 6]
+    score[0] = 457;
+    score[1] = 245;
+    score[2] = 532;
 
     // If I involve only one decimal point in denominator it will promote the whole arithmetic expression to being floating point.
     printf("Average: %f\n", (score[0] + score[1] + score[2]) / 3.0);
