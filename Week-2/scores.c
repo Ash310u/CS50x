740af911ee446f9a06ba4d944cb98178c0e00c30
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-04 03:05:59 +0000

    Sat, May 4, 2024, 8:35 AM +05:30

--- Week-2/scores.c
+++ Week-2/scores.c
@@ -7,5 +7,5 @@ int main (void)
     int score3 = 342;
 
     // If I involve only one decimal point in denominator it will promote the whole arithmetic expression to being floating point.
-    printf("Average: %f%d\n", (score1 + score2 + score3) / (double) 3.0);
+    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
 }
