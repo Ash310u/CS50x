a0e14618aefabb6ee913b747ff6b3fda23162bd8
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-04 10:31:13 +0000

    Sat, May 4, 2024, 4:01 PM +05:30

--- Week-2/status.c
+++ Week-2/status.c
@@ -1,19 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main (int argc, string argv[])
+int main(int argc, string argv[])
 {
-    for(int i = 0; i < argc; i++)
+    if (argc != 2)
     {
-        if(argc != 2) {
-            printf("Missing command-line argument.\n");
-            return 1;
-        }
-
-        if(i > 0)
-        {
-            printf("%s ", argv[i]);
-        }
+        printf("Missing command-line argument.\n");
+        return 1;
     }
-    printf("\n");
+    printf("%s\n", argv[1]);
+    return 0;
 }
