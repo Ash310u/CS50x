dbd07bcc530f8056eb6c1efe3c56469cd50d84c6
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-04 04:01:04 +0000

    Sat, May 4, 2024, 9:31 AM +05:30

--- Week-2/scores.c
+++ Week-2/scores.c
@@ -2,11 +2,15 @@
 #include <stdio.h>
 
 int main (void)
-{               // jsut trying things.
+
+{
+    int n = get_int("Scores Types: ");
+                   // jsut trying things.
     int score[3]; // = [3, 5, 6]
-    score[0] = get_int("Score1: ");
-    score[1] = get_int("Score2: ");
-    score[2] = get_int("Score3: ");
+    for(int i = 0; i < n; i++)
+    {
+        score[i] = get_int("Score2: ");
+    }
 
     // If I involve only one decimal point in denominator it will promote the whole arithmetic expression to being floating point.
     printf("Average: %f\n", (score[0] + score[1] + score[2]) / 3.0);
