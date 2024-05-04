3b2d4bf0ae8f70e0ee6ac0602ab6b9053abf0258
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-04 07:42:40 +0000

    Sat, May 4, 2024, 1:12 PM +05:30

--- Week-2/string.c
+++ Week-2/string.c
@@ -1,1 +1,13 @@
-#inC
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main (void)
+{
+    string s = get_string("Input: ");
+    printf("Output: ");
+    for (int i = 0; i < strlen(s); i++)
+    {
+        printf("%c", s[i])
+    }
+}
