5ccf0447c4955c90b3f8423aa20745c16d51705e
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-05 00:51:43 +0000

    Sun, May 5, 2024, 6:21 AM +05:30

--- Week-1/mario.c
+++ Week-1/mario.c
@@ -62,9 +62,9 @@ void print_right(int length, int space_length)
 void print_spaced_row(int length, int space_length)
 {
     // second loop will print Horizontal
-    for (int j = 0; j <= length - space_length; j++)
+    for (int j = length; j >= space_length; j--)
     {
-        if (space_length <= j)
+        if (space_length >= j)
         {
             printf("*");
         }
