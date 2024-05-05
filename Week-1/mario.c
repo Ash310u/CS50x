6001b2d67139eaf8efd2deaca64d33b0486bd93a
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-05 01:08:39 +0000

    Sun, May 5, 2024, 6:38 AM +05:30

--- Week-1/mario.c
+++ Week-1/mario.c
@@ -3,25 +3,23 @@
 
 void print_left(int length, int space_length);
 void print_right(int length, int space_length);
-void print_spaced_row(int length, int space_length);
+// void print_spaced_row(int length, int space_length);
 int main(void)
 {
     // Promt User for positive integer
-    int n = 5;
-    // do
-    // {
-    //     n = get_int("Size: ");
-    // }
-    // while (n < 1);
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
 
     // Print an n-by-n grid of bricks
     // first loop will print Vertical
     for (int i = 0; i <= n; i++)
     {
-        // print_left(n,i);
-        // printf("    ");
-        // print_right(n,i);
-        print_spaced_row(n,i);
+        print_left(n,i);
+        print_right(n,i);
         printf("\n");
     }
 }
@@ -59,18 +57,19 @@ void print_right(int length, int space_length)
     }
 }
 
-void print_spaced_row(int length, int space_length)
-{
-    // second loop will print Horizontal
-    for (int j = 0; j <= length ; j++)
-    {
-        if (length - space_length <= j)
-        {
-            printf("*");
-        }
-        else
-        {
-            printf(".");
-        }
-    }
-}
+// Please IGNORE this, I'm jsut trying to do somtihng...
+// void print_spaced_row(int length, int space_length)
+// {
+//     // second loop will print Horizontal
+//     for (int j = 0; j <= length ; j++)
+//     {
+//         if (length - space_length <= j)
+//         {
+//             printf("*");
+//         }
+//         else
+//         {
+//             printf(".");
+//         }
+//     }
+// }
