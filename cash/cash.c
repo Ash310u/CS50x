2ff561017df620ecb880c4332bbaa759679432c8
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-06 05:30:02 +0000

    Mon, May 6, 2024, 11:00 AM +05:30

--- cash/cash.c
+++ cash/cash.c
@@ -6,17 +6,19 @@ int main(void)
     int n;
     do {
         n = get_int("Change Owed: ");
-    } while (n > 0);
+    } while (n < 0);
 
     int coins = 0;
     do {
-        if(n > 25) {
+        if(n >= 25) {
             n = n - 25;
-            coins ++;
+            coins = coins + 1 ;
         }
         // else if(n > 25) {
             // coins ++;
         // }
     } while (n == 0);
+
+    printf("%i\n", n);
     printf("%i\n", coins);
 }
