6164ed69ae88c6dbba8b9261d7f9307f0e7ba7f5
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-06 05:21:19 +0000

    Mon, May 6, 2024, 10:51 AM +05:30

---
+++ cash/cash.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do {
+        n = get_int("Change Owed: ");
+    } while (n > 0);
+
+    int coins;
+    do {
+        if(n > 25) {
+            coins ++;
+        }
+        // else if(n > 25) {
+            // coins ++;
+        // }
+    } while ()
+
+}
