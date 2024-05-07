f0d704a4d0bb74f5d7c34e1bf041aba9a6c831d8
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-07 08:10:02 +0000

    Tue, May 7, 2024, 1:40 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -10,43 +10,46 @@ int main(void)
     }
     while (n <= 0);
 
-    long org_n = n;
-    int num_digit = 0;
+    int sum = 0;
+    
 
-    while (n > 0)
-    {
-        n = n / 10;
-        num_digit++;
-    }
-    n = org_n;
+    // long org_n = n;
+    // int num_digit = 0;
 
-    int arr[num_digit];
-    int sub_total = 0;
-    for (int i = num_digit - 1; i >= 0; i--)
-    {
-        arr[i] = n % 10;
-        if (i % 2 != 0)
-        {
-            int digit = arr[i] * 2;
-            if (digit > 9)
-            {
-                while (digit > 0)
-                {
-                    sub_total += digit % 10;
-                    digit = digit / 10;
-                }
-            }
-            else
-            {
-                sub_total += digit;
-            }
-        }
-        else
-        {
-            sub_total += arr[i];
-        }
-        n = n / 10;
-    }
+    // while (n > 0)
+    // {
+    //     n = n / 10;
+    //     num_digit++;
+    // }
+    // n = org_n;
+
+    // int arr[num_digit];
+    // int sub_total = 0;
+    // for (int i = num_digit - 1; i >= 0; i--)
+    // {
+    //     arr[i] = n % 10;
+    //     if (i % 2 != 0)
+    //     {
+    //         int digit = arr[i] * 2;
+    //         if (digit > 9)
+    //         {
+    //             while (digit > 0)
+    //             {
+    //                 sub_total += digit % 10;
+    //                 digit = digit / 10;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             sub_total += digit;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         sub_total += arr[i];
+    //     }
+    //     n = n / 10;
+    // }
 
     if (sub_total % 10 == 0)
     {
