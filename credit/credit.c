cd018396d69c60389fadb667bd931bdc712d82af
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-07 05:36:05 +0000

    Tue, May 7, 2024, 11:06 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -20,29 +20,32 @@ int main(void)
     }
     n = org_n;
 
-    printf("Length: %i\n", num_digit);
+    // printf("Length: %i\n", num_digit);
 
     int arr[num_digit];
     int sub_total = 0;
     for (int i = num_digit - 1; i >= 0; i--) {
         arr[i] = n % 10;
-        printf("arr[%i]: %i\n", i ,arr[i]);
+        // printf("arr[%i]: %i\n", i ,arr[i]);
         if (i == 0 || i % 2 != 0)
         {
             int digit = arr[i] * 2;
             if(digit > 9)
             {
-                sub_total += digit % 10;
-                digit = digit / 10;
+                while (digit > 0)
+                {
+                    sub_total += digit % 10;
+                    digit = digit / 10;
+                }
             }
             else
             {
                 sub_total += digit;
             }
-            printf("%i\n", digit);
+            printf("Digit: %i\n", digit);
         }
         n = n / 10;
     }
-    printf("%i\n", sub_total);
+    printf("Sub Total: %i\n", sub_total);
 
 }
