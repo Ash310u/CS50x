c5398356a4a2a190dec0d8dbf0622b173fa9adf0
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-07 09:15:49 +0000

    Tue, May 7, 2024, 2:45 PM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -29,63 +29,27 @@ int main(void)
         sum += (multi % 10) + (multi / 10);
         org_n = org_n / 100;
     }
-    // int num_digit = 0;
 
-    // while (n > 0)
-    // {
-    //     n = n / 10;
-    //     num_digit++;
-    // }
-    // n = org_n;
-
-    // int arr[num_digit];
-    // int sub_total = 0;
-    // for (int i = num_digit - 1; i >= 0; i--)
-    // {
-    //     arr[i] = n % 10;
-    //     if (i % 2 != 0)
-    //     {
-    //         int digit = arr[i] * 2;
-    //         if (digit > 9)
-    //         {
-    //             while (digit > 0)
-    //             {
-    //                 sub_total += digit % 10;
-    //                 digit = digit / 10;
-    //             }
-    //         }
-    //         else
-    //         {
-    //             sub_total += digit;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         sub_total += arr[i];
-    //     }
-    //     n = n / 10;
-    // }
-
-    // if (sub_total % 10 == 0)
-    // {
-    //     if (num_digit == 15 && arr[15] == 3)
-    //     {
-    //         if (arr[14] == 4 || arr[14] == 7)
-    //         {
-    //             printf("AMEX\n");
-    //         }
-    //     }
-    //     else if (num_digit == 16 && arr[16] == 5)
-    //     {
-    //         if (arr[15] == 1 || arr[15] == 2 || arr[15] == 3 || arr[15] == 4 || arr[15] == 5)
-    //         {
-    //             printf("MASTERCARD\n");
-    //         }
-    //     }
-    //     printf("VISA\n");
-    // }
-    // else
-    // {
-    //     printf("INVALID \n");
-    // }
+    if (sum % 10 == 0)
+    {
+        if (num_digit == 15 && arr[15] == 3)
+        {
+            if (arr[14] == 4 || arr[14] == 7)
+            {
+                printf("AMEX\n");
+            }
+        }
+        else if (num_digit == 16 && arr[16] == 5)
+        {
+            if (arr[15] == 1 || arr[15] == 2 || arr[15] == 3 || arr[15] == 4 || arr[15] == 5)
+            {
+                printf("MASTERCARD\n");
+            }
+        }
+        printf("VISA\n");
+    }
+    else
+    {
+        printf("INVALID \n");
+    }
 }
