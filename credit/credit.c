0523b87f26e30a3cabb4c91ca146c1bf4a4e79d3
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-11 03:55:59 +0000

    Sat, May 11, 2024, 9:25 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -32,19 +32,22 @@ int main(void)
     }
 
     org_n = n;
-    while(org_n < 0)
+    while(org_n >= 0)
     {
-
+        org_n = org_n / 10;
+        count++;
+        printf("%i\n", count);
     }
+    printf("%i\n", count);
 
     if (sum % 10 == 0)
     {
         printf("VISA\n");
     }
-    if ()
-    {
+    // if ()
+    // {
 
-    }
+    // }
     else
     {
         printf("INVALID \n");
