035468051f62622572a625dcf84cee3b8d1402bd
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-11 03:36:42 +0000

    Sat, May 11, 2024, 9:06 AM +05:30

--- credit/credit.c
+++ credit/credit.c
@@ -15,19 +15,19 @@ int main(void)
     int sum = 0;
     while (org_n > 0)
     {
-        int lastDigit = org_n % 10;
-        sum += lastDigit;
-        org_n = org_n / 100;
+        int lastDigit = org_n % 10; 123456789
+        sum += lastDigit; 9 + 7 + 5 + 3 + 1 = 25
+        org_n = org_n / 100; // 0
     }
 
-    org_n = n / 10;
+    org_n = n / 10; 12345678
 
     while (org_n > 0)
     {
-        int lastDigit = org_n % 10;
-        int multi = lastDigit * 2;
-        sum += (multi % 10) + (multi / 10);
-        org_n = org_n / 100;
+        int lastDigit = org_n % 10; 1234
+        int multi = lastDigit * 2; 8*2, 6*2, 4*2
+        sum += (multi % 10) + (multi / 10); 25 + (6 + 1) + (2 + 1) + (8 + )
+        org_n = org_n / 100; 1234
     }
 
     if (sum % 10 == 0)
