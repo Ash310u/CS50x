1d1885bfa16506b5ca898c52ed8cca99bbdd5d40
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-12 08:16:35 +0000

    Sun, May 12, 2024, 1:46 PM +05:30

--- Week-3/search.c
+++ Week-3/search.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main (void) {
-    int number[] = {10,200,100,25,50,5,500};
+    int numbers[] = {10,200,100,25,50,5,500};
 
     int n = get_int("Number: ");
     for(int i = 0; i < 7; i++)
     {
-        if( number[i] == n)
+        if( numbers[i] == n)
         {
             printf("Found\n");
             return 0;
