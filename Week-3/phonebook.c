1c1ff41724df82a8a228c290f351615e0b83ec17
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-12 09:51:08 +0000

    Sun, May 12, 2024, 3:21 PM +05:30

--- Week-3/phonebook.c
+++ Week-3/phonebook.c
@@ -4,5 +4,18 @@
 
 int main (void)
 {
+    string names[] = {"Ashu", "Ritwik", "Ypk"};
+    string numbers[] = {"+91-765-585-2770", "+91-825-585-7848", "+91-765-585-7848"};
 
+    string s = get_string("Name: ");
+    for(int i = 0; i < 3; i++)
+    {
+        if(strcmp(names[i], s)==0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not Found\n");
+    return 1;
 }
