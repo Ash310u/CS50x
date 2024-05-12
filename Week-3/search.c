af695da699c91e57dc7e35b929f662d754067e8d
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-12 08:28:52 +0000

    Sun, May 12, 2024, 1:58 PM +05:30

--- Week-3/search.c
+++ Week-3/search.c
@@ -9,7 +9,7 @@ int main (void) {
 
     for(int i = 0; i < 6; i++)
     {
-        if(strcmp(strings[i],s))
+        if(strcmp(strings[i],s) == 0)
         {
             printf("Found\n");
             return 0;
