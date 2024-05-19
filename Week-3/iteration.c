f22f1dd83a898c77f368d1504d9be034584f7e97
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-19 15:34:30 +0000

    Sun, May 19, 2024, 9:04 PM +05:30

--- Week-3/iteration.c
+++ Week-3/iteration.c
@@ -1,2 +1,20 @@
 #include <cs50.h>
 #include <stdio.h>
+
+void draw(int n);
+int main(void)
+{
+    int height = get_int("Height: ");
+    draw(height);
+}
+
+void draw(int n)
+{
+    for(int i = 0; i < n ; i++)
+    {
+        for(int j = n; j > 0 ; j--) {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
