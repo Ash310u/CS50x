b801f1dda0ea478219db1791b141656043343fe7
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-21 16:27:39 +0000

    Tue, May 21, 2024, 9:57 PM +05:30

--- Week-3/iteration.c
+++ Week-3/iteration.c
@@ -12,7 +12,7 @@ void draw(int n)
 {
     for(int i = 0; i < n ; i++)
     {
-        for(int j = 0; j < i+1; j++) {
+        for(int j = 0; j < n-i; j++) {
             printf("#");
         }
         printf("\n");
