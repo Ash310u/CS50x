0230e45efad46bfa6d89e2de8693b716325eae39
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-25 19:58:57 +0000

    Sun, May 26, 2024, 1:28 AM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -4,20 +4,36 @@
 #include <stdio.h>
 #include <string.h>
 
-int count_letters(string text);
-int define_grade(string text);
+int letters_count(string text);
+int words_count(string text);
+int sentance_count(string text);
+int grade_count(string text);
+
 int main(void)
 {
     string text = get_string("Text: ");
 
+    int num_letters = count_words(text);
+    int num_words = count_words(text);
+
 }
 
-int count_letters(string text)
+int letters_count(string text)
 {
     // Return the number of letters in text
 }
 
-int define_grade(int L S)
+int words_count(string text)
+{
+    // Return the number of words in text
+}
+
+int sentance_count(string text)
+{
+    // Return the number of words in text
+}
+
+int grade_count(int L, int S)
 {
 
     int grade = 0.0588 * L - 0.296 * S - 15.8
