f28642ac92a4583c7490fa057817bdd61a4a18c1
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-25 16:37:17 +0000

    Sat, May 25, 2024, 10:07 PM +05:30

---
+++ scrabble/scrabble.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for two words
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Compute the score of each word
+    
+
+    // Print the winner
+}
