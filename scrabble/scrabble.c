d0b9a0acd0dade22baea9d96ee3262bad5e41de6
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-25 16:53:24 +0000

    Sat, May 25, 2024, 10:23 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -10,7 +10,14 @@ int main(void)
     string word2 = get_string("Player 2: ");
 
     // Compute the score of each word
-    const letter[] = {'a','b','c',}
+    const points[] = {
+        1, 3, 3, 2, 1, 4, 2,
+        4, 1, 8, 5, 1, 3, 1,
+        1, 3, 10, 1, 1, 1, 1,
+        4, 4, 8,4, 10
+    };
+
+    
 
     // Print the winner
 }
