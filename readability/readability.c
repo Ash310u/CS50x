c0e4e7b0b556e723780bf31dcc43eb9ba18a0429
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-25 19:51:30 +0000

    Sun, May 26, 2024, 1:21 AM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,8 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <cs50.h>
 
 int define_grade(string text);
 int main(void)
@@ -11,6 +13,6 @@ int main(void)
 
 int define_grade(string text)
 {
-    
+
     int grade = 0.0588 * L - 0.296 * S - 15.8
 }
