3d05caa9c58d04cca867fa8e5be458560e41c45e
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-25 16:56:28 +0000

    Sat, May 25, 2024, 10:26 PM +05:30

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <string.h>
 
+const POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+
 int main(void)
 {
     // Prompt the user for two words
@@ -10,14 +13,6 @@ int main(void)
     string word2 = get_string("Player 2: ");
 
     // Compute the score of each word
-    const points[] = {
-        1, 3, 3, 2, 1, 4, 2,
-        4, 1, 8, 5, 1, 3, 1,
-        1, 3, 10, 1, 1, 1, 1,
-        4, 4, 8,4, 10
-    };
-
-
 
     // Print the winner
 }
