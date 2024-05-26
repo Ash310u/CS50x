c7014774133bee4a283fdc8afd468f363f50d2c2
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-26 16:15:39 +0000

    Sun, May 26, 2024, 9:45 PM +05:30

--- readability/readability.c
+++ readability/readability.c
@@ -82,5 +82,7 @@ int sentance_count(string text)
 int grade_count(int L, int S)
 {
     float grade = roundf(0.0588 * L - 0.296 * S - 15.8);
+    float g = (0.0588 * L - 0.296 * S - 15.8);
+    printf("%f, %f\n",grade, g);
     return grade;
 }
