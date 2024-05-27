37fb58e9e262e78a070f87011e286276664a4abd
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-27 15:13:13 +0000

    Mon, May 27, 2024, 8:43 PM +05:30

--- Week-4/addresses.c
+++ Week-4/addresses.c
@@ -5,8 +5,15 @@ int main(void)
     int n = 50;
     int *p = &n;
 
-    char s = l;
-
+    // string : char with "*" which store the address of the first character of a string & '\0' indicates the end of a string.
+    char *s = "Hi!";
     printf("%p\n",  s);
     printf("%p\n",  &s[0]);
+
+    printf("-------------------\n");
+
+    // char : just a single character.
+    char g = 'G';
+    printf("%p\n",  &g);
+    printf("%p\n",  &g);
 }
