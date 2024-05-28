f5409c1d2986a217d2829ae35965ce749b118c94
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-28 06:57:44 +0000

    Tue, May 28, 2024, 12:27 PM +05:30

--- Week-4/copy.c
+++ Week-4/copy.c
@@ -12,14 +12,14 @@ int main(void)
         return 1;
     }
     // char *t = s; // The Assiegnment(=) operator literarily copy the address on the 's' pointer over to 't' pointer. Both pointed at the same string location in memory.
-    // We have '+ 1' because the 'strlen()' returns human visual length not the actual length with counting '\0'.
+    // We have '+ 1' because the 'strlen()' returns human visual length not the actual length with counting '\0'(NUL).
     char *t = malloc(strlen(s) + 1); // malloc(Memory Allocation) basically it Allocate 'some numbers(which we provied)' of bytes in the memory.
-    if(t == NULL)
+    if(t == NULL) // 'NULL' it is an address and it's literarily the address ZERO(0).
     {
         return 1;
     }
 
-    // We have to set 'i <= n' not 'i < n', Because the 'strlen()' returns human visual length not the actual length with counting '\0'.
+    // We have to set 'i <= n' not 'i < n', Because the 'strlen()' returns human visual length not the actual length with counting '\0'(NUL).
     // for(int i = 0, n = strlen(s); i <= n; i++)
     // {
     //     t[i] = s[i]; // setting up the data of the string 's' to 't' at a different memory location which is allocated by the 'malloc' function.
