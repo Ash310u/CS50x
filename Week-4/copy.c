94d9a9eb7fd91b52147916a62b0e3f37fef350aa
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-28 06:01:59 +0000

    Tue, May 28, 2024, 11:31 AM +05:30

--- Week-4/copy.c
+++ Week-4/copy.c
@@ -9,7 +9,12 @@ int main(void)
     char *s = get_string("s: ");
 
     // char *t = s; // The Assiegnment(=) operator literarily copy the address on the 's' pointer over to 't' pointer. Both pointed at the same string location in memory.
-    char *t = malloc(strlen(s)) // malloc(Memory Allocation) basically it Allocate 'some numbers(which we provied)' of bytes in the memory
+    char *t = malloc(strlen(s)); // malloc(Memory Allocation) basically it Allocate 'some numbers(which we provied)' of bytes in the memory.
+
+    for(int i = 0, n = strlen(s); i < n; i++)
+    {
+        t[i] = s[i]; // setting up the data of the string 's' to 't' at a different memory location which is allocated by the 'malloc' function.
+    }
 
     if(strlen(t) > 0) {
         t[0] = toupper(t[0]);
