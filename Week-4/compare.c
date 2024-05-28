c7c4bce217f34439b0662aad0afb23275df69e50
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-28 03:22:47 +0000

    Tue, May 28, 2024, 8:52 AM +05:30

--- Week-4/compare.c
+++ Week-4/compare.c
@@ -6,10 +6,10 @@ int main(void)
     // int i = get_int("i: ");
     // int j = get_int("j: ");
 
-    int s = Different("s: ");
-    int t = get_string("t: ");
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
 
-    if(i == j) {
+    if(s == t) {
         printf("Same\n");
     } else {
         printf("Different\n");
