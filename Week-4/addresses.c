889ee00880c7c0029c92a1e104b42fde46ad9663
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-05-28 03:13:31 +0000

    Tue, May 28, 2024, 8:43 AM +05:30

--- Week-4/addresses.c
+++ Week-4/addresses.c
@@ -9,7 +9,14 @@ int main(void)
     char *s = "Hi!";
     printf("%p\n",  s);
     printf("%p\n",  &s[0]);
-    printf("%p\n",  *(s + 1));
+
+    printf("%c\n",  s[1]);
+    printf("-------------------\n");
+    // Pointer Arithmetic(Basically Math on Addresses)
+    printf("%c",  *s);
+    printf("%c",  *(s + 1));
+    printf("%c",  *(s + 2));
+    printf("%i\n", *(s + 3));
 
     printf("-------------------\n");
 
