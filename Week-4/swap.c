8c7acbc7ae0937e5d1a409a2a4ca7e6f8c5af276
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-06-09 22:25:16 +0000

    Mon, Jun 10, 2024, 3:55 AM +05:30

--- Week-4/swap.c
+++ Week-4/swap.c
@@ -5,8 +5,10 @@ int main(void)
     // int x = 1;
     // int y = 2;
     // int x;
-    int x;
-    printf("somthing %n\n", &x);
+    int count;
+    printf("Hello, World!%n", &count); // &count is the address of the variable count
+    // printf("\nCharacters printed: %d\n", count);
+    return 0;
     // int *p = &x;
     // printf("Before Swapping x = %p, y = %p \n", &x, p);
 }
