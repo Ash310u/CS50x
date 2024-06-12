6b52714a5704fba0b203fdfc4fd9307541715880
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-06-12 02:54:05 +0000

    Wed, Jun 12, 2024, 8:24 AM +05:30

--- Week-4/get.c
+++ Week-4/get.c
@@ -2,9 +2,13 @@
 
 int main(void)
 {
-    char *s;
-    // char s[4];
-    printf("s: ");
-    scanf("%s", s);
-    printf("s: %s\n", s);
+    // char *s;
+    // printf("s: ");
+    // scanf("%s", s);
+    // printf("s: %s\n", s);
+
+    char c;
+    printf("c: ");
+    scanf("%c", c);
+    printf("c: %c\n", c);
 }
