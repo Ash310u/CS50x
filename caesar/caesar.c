67fb6ca0ed6e240e332c565c47fdf3a3c2d92fbd
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-06-15 12:37:49 +0000

    Sat, Jun 15, 2024, 6:07 PM +05:30

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,16 +1,18 @@
+#include <cs50.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
 int main(int argc , char *argv[])
 {
-    int i = 0;
-    if (argv[1] && atoi(argv[1]) == 0 )
+    if (!argv[1] || atoi(argv[1]) == 0 )
     {
-        i = atoi(argv[1]);
-        printf("%i\n", i);
+    printf("Usage: ./caesar key\n");
         return 0;
     }
-    printf("Usage: ./caesar key\n");
+
+    int i = atoi(argv[1]);
+    char *sentance = get_string("plaintext: ")
+    while ()
 
 }
