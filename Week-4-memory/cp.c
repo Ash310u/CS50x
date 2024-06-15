40f68508e8c197d40dec35ff32882ca97c1121d9
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-06-15 06:23:40 +0000

    Sat, Jun 15, 2024, 11:53 AM +05:30

--- Week-4-memory/cp.c
+++ Week-4-memory/cp.c
@@ -1,21 +1,51 @@
-#include <stdio.h>
 #include <stdint.h>
+#include <stdio.h>
 
 typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
-    FILE *src = fopen(argv[1], "rb");
-    FILE *dst = fopen(argv[2], "wb");
+    // FILE *src = fopen(argv[1], "rb");
+    // FILE *dst = fopen(argv[2], "wb");
 
-    BYTE b;
+    // BYTE b;
 
-    while (fread(&b, sizeof(b), 1, src) != 0)
+    // while (fread(&b, sizeof(b), 1, src) != 0)
+    // {
+    //     fwrite(&b, sizeof(b), 1, dst);
+    // };
+
+    // fclose(src);
+    // fclose(dst);
+
+    FILE *file = fopen(argv[1], "rb");
+    if (!file)
     {
-        fwrite(&b, sizeof(b), 1, dst);
-    };
+        perror("File opening failed");
+        return 1;
+    }
 
-    fclose(src);
-    fclose(dst);
+    size_t result;
+    int buffer[10];
 
+    result = fread(buffer, sizeof(int), 10, file);
+    if (result != 10)
+    {
+        if (feof(file))
+        {
+            printf("End of file reached.\n");
+        }
+        else if (ferror(file))
+        {
+            perror("Error reading from file");
+        }
+    }
+    else
+    {
+        printf("result: %zu\n", result);
+        printf("Successfully read 10 integers.\n");
+    }
+
+    fclose(file);
+    return 0;
 }
