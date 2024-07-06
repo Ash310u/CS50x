bace469d65978bf2612d5dda89d568059bf41242
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-07-06 10:44:11 +0000

    Sat, Jul 6, 2024, 4:14 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -12,16 +12,19 @@ int main(int argc, char *argv[])
 
             if (n <= 25)
             {
-                 if (isalpha((unsigned char) argv[1][n]) == 0)
+                if (isalpha((unsigned char) argv[1][n]) == 0)
                 {
                     printf("Usage: ./substitution key should be Alphabet \n");
                     return 1;
                 }
                 n++;
+                if (argv[1][n] != '\0')
+                {
+                    cipherText = argv[1];
+                }
             }
         }
         while (n <= 25);
-        cipherText = argv[1];
     }
     else
     {
