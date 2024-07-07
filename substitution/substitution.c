f77b5c2d47c70bfed052cc561e02f5cc1c9bfc84
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-07-07 18:07:20 +0000

    Sun, Jul 7, 2024, 11:37 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,38 +3,35 @@
 
 int main(int argc, char *argv[])
 {
-    char *cipherText[26];
+    char *cipherText[];
     if (argc == 2)
     {
         int n = 0;
+        if (argv[1][26] != '\0')
+        {
+            printf("Usage: ./substitution key should be 26 character\n");
+        }
         do
         {
 
-            if (n <= 25)
+            if (isalpha((unsigned char) argv[1][n]) == 0)
             {
-                if (isalpha((unsigned char) argv[1][n]) == 0)
-                {
-                    printf("Usage: ./substitution key should be Alphabet \n");
-                    return 1;
-                }
-                n++;
-                if (argv[1][n] == '\0')
-                {
-                    printf("Usage: ./substitution key should be 26 character\n");
-                }
+                printf("Usage: ./substitution key should be Alphabet \n");
+                return 1;
             }
+            n++;
         }
         while (n <= 25);
-        if (argv[1][25] == '\0')
-        {
+        // if (argv[1][25] == '\0')
+        // {
             cipherText = argv[1];
-        }
+        // }
     }
     else
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
-    printf("value: %s\n", cipherText);
+    printf("value: %s\n", *cipherText);
     return 0;
 }
