3f6e01783b4e551351df4a8d202ed587c2f03697
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-07-08 12:07:59 +0000

    Mon, Jul 8, 2024, 5:37 PM +05:30

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     char *p = &argv[1][0];
-    char *cipherText[26];
+    char *substitution[26];
     if (argc == 2)
     {
         int n = 0;
@@ -22,10 +22,10 @@ int main(int argc, char *argv[])
                 printf("Usage: ./substitution key should be Alphabet \n");
                 return 1;
             }
-            *(cipherText + n) = &p[n];
+            *(substitution + n) = &p[n];
             for (int i = n + 1; i <= n + 1; i++)
             {
-                if (tolower(*cipherText[n]) == tolower(argv[1][i]))
+                if (tolower(*substitution[n]) == tolower(argv[1][i]))
                 {
                     printf("Usage: 26 Unique Alphabet\n");
                     return 1;
@@ -45,7 +45,8 @@ int main(int argc, char *argv[])
                              "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
     char *plainText = get_string("plaintext: ");
-
+    // char *x = &plainText[0];
+    char *cipherText[strlen(plainText)];
     for (int i = 0, length = strlen(plainText) - 1; i <= length; i++)
     {
         for (int n = 0; n <= 25; n++)
@@ -54,19 +55,22 @@ int main(int argc, char *argv[])
             {
                 if (plainText[i] >= 65 && plainText[i] <= 90)
                 {
-                    printf("%c", toupper(*cipherText[n]));
+                    // printf("%c", toupper(*substitution[n]));
+                    *cipherText[i] = toupper(*substitution[n]);
                 }
                 else if (plainText[i] >= 97 && plainText[i] <= 122)
                 {
-                    printf("%c", tolower(*cipherText[n]));
+                    // printf("%c", tolower(*substitution[n]));
+                    *cipherText[i] = tolower(*substitution[n]);
                 }
             }
         }
         if ((plainText[i] < 65 || plainText[i] > 90) && (plainText[i] < 97 || plainText[i] > 122))
         {
             printf("%c", plainText[i]);
+            *cipherText[i] = plainText[i];
         }
     }
-    printf(" \n");
+    printf("%s\n", *cipherText);
     return 0;
 }
