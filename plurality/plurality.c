240a57222ceab8262ce6cda775e3806880144d80
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-07-28 14:59:15 +0000

    Sun, Jul 28, 2024, 8:29 PM +05:30

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -92,6 +92,10 @@ void print_winner(void)
             } else if (candidates[i].votes < candidates[n].votes)
             {
                 printf("%s\n", candidates[n].name);
+            } else if (candidates[i].votes == candidates[n].votes)
+            {
+                printf("%s\n", candidates[i].name);
+                printf("%s\n", candidates[n].name);
             }
         }
     }
