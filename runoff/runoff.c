ae897746508cf189b667bd1e4be327d1ec459c39
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-07-30 04:59:47 +0000

    Tue, Jul 30, 2024, 10:29 AM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -128,7 +128,10 @@ bool vote(int voter, int rank, string name)
 {
     for(int i = 0; i < candidate_count; i++)
     {
-        
+        if (strcmp(candidates[i], name))
+        {
+            
+        }
     }
     return false;
 }
