cbb721c7b3edbec9319243b4358bee1334a4927b
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-07-30 05:59:23 +0000

    Tue, Jul 30, 2024, 11:29 AM +05:30

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -141,14 +141,16 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = 0; i < voter_count; i++)
     {
-        if (!candidates[i].eliminated)
+        for (int j = 0; j < candidate_count; j++)
         {
-            candidates[i].votes++;
+            if (!candidates[preferences[i][j]].eliminated)
+            {
+                candidates[preferences[i][j]].votes++;
+            }
         }
     }
-    // TODO
     return;
 }
 
