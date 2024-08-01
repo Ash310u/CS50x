1b3373549569a1df4c2e15b46a065869ab3ba584
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-08-01 11:01:55 +0000

    Thu, Aug 1, 2024, 4:31 PM +05:30

--- volume/volume.c
+++ volume/volume.c
@@ -37,7 +37,10 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    while (fread(&HEADER_SIZE, sizeof(HEADER_SIZE), 1, input) <= 44 )
+    while (fread(&HEADER_SIZE, sizeof(HEADER_SIZE), 1, input) <= 44)
+    {
+        fwrite(&, sizeof(b), 1, dst);
+    }
 
     // TODO: Read samples from input file and write updated data to output file
 
