838d59ca3507d915815c6e39afbdb33b69a85163
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-08-21 13:27:27 +0000

    Wed, Aug 21, 2024, 6:57 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -4,19 +4,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    for(int n = 0; n <= height; n++)
+    for (int n = 0; n < height; n++)
     {
-        for(int i = 0; i <= height; i++)
+        for (int i = 0; i < width; i++)
         {
-            int avg = round(image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed);
-            
+            int avg = round((image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed) / 3.0);
+
             image[n][i].rgbtBlue = avg;
             image[n][i].rgbtGreen = avg;
             image[n][i].rgbtRed = avg;
         }
-
     }
-    return;
 }
 
 // Convert image to sepia
