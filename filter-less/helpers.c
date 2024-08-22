55912191ea4869059a559ee3054d57251f0b97c0
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-08-22 13:45:41 +0000

    Thu, Aug 22, 2024, 7:15 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -15,11 +15,23 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[n][i].rgbtRed = avg;
         }
     }
+    return;
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int n = 0; n < height; n++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            int avg = round((image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed) / 3.0);
+
+            image[n][i].rgbtBlue = avg;
+            image[n][i].rgbtGreen = avg;
+            image[n][i].rgbtRed = avg;
+        }
+    }
     return;
 }
 
