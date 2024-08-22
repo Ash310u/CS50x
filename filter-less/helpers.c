0ccb34db54cbe4ec087824648c0f9fc42966fbc3
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-08-22 14:22:44 +0000

    Thu, Aug 22, 2024, 7:52 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -65,6 +65,17 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int n = 0; n < height; n++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            int avg = round((image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed) / 3.0);
+
+            image[n][i].rgbtRed = avg;
+            image[n][i].rgbtGreen = avg;
+            image[n][i].rgbtBlue = avg;
+        }
+    }
     return;
 }
 
