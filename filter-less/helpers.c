e7d99ede5b619986c4bc3b9e3f333d0fe55a3912
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-08-22 16:03:51 +0000

    Thu, Aug 22, 2024, 9:33 PM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -9,7 +9,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int i = 0; i < width; i++)
         {
-            int avg = round((image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed) / 3.0);
+            int avg =
+                round((image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed) / 3.0);
 
             image[n][i].rgbtRed = avg;
             image[n][i].rgbtGreen = avg;
@@ -26,30 +27,36 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int i = 0; i < width; i++)
         {
-            int sepiaRed = round(.393 * image[n][i].rgbtRed + .769 * image[n][i].rgbtGreen + .189 * image[n][i].rgbtBlue);
-            int sepiaGreen = round(.349 * image[n][i].rgbtRed + .686 * image[n][i].rgbtGreen + .168 * image[n][i].rgbtBlue) ;
-            int sepiaBlue = round(.272 * image[n][i].rgbtRed + .534 * image[n][i].rgbtGreen + .131 * image[n][i].rgbtBlue) ;
+            int sepiaRed = round(.393 * image[n][i].rgbtRed + .769 * image[n][i].rgbtGreen +
+                                 .189 * image[n][i].rgbtBlue);
+            int sepiaGreen = round(.349 * image[n][i].rgbtRed + .686 * image[n][i].rgbtGreen +
+                                   .168 * image[n][i].rgbtBlue);
+            int sepiaBlue = round(.272 * image[n][i].rgbtRed + .534 * image[n][i].rgbtGreen +
+                                  .131 * image[n][i].rgbtBlue);
 
-            if(sepiaRed <= 0)
+            if (sepiaRed <= 0)
             {
                 sepiaRed = 0;
-            } else if(sepiaRed >= 255)
+            }
+            else if (sepiaRed >= 255)
             {
                 sepiaRed = 255;
             }
 
-            if(sepiaGreen <= 0)
+            if (sepiaGreen <= 0)
             {
                 sepiaGreen = 0;
-            } else if(sepiaGreen >= 255)
+            }
+            else if (sepiaGreen >= 255)
             {
                 sepiaGreen = 255;
             }
 
-            if(sepiaBlue <= 0)
+            if (sepiaBlue <= 0)
             {
                 sepiaBlue = 0;
-            } else if(sepiaBlue >= 255)
+            }
+            else if (sepiaBlue >= 255)
             {
                 sepiaBlue = 255;
             }
@@ -57,7 +64,6 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             image[n][i].rgbtRed = sepiaRed;
             image[n][i].rgbtGreen = sepiaGreen;
             image[n][i].rgbtBlue = sepiaBlue;
-
         }
     }
     return;
@@ -68,7 +74,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int n = 0; n < height; n++)
     {
-        for (int i = 0, w = width / 2 ; i < w ; i++)
+        for (int i = 0, w = width / 2; i < w; i++)
         {
             int swapRed = image[n][i].rgbtRed;
             int swapGreen = image[n][i].rgbtGreen;
@@ -91,10 +97,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 {
     for (int n = 0; n < height; n++)
     {
-        for (int n = 0; n < 3; n++)
+        for (int i = 0; i < width; i++)
         {
-            for (int i = 0; i < width; i++)
+            for (int n = 0; n < 3; n++)
             {
+                int avg = round((image[n][i].rgbtBlue + image[n][i].rgbtGreen + image[n][i].rgbtRed) / 3.0);
                 image[n][i].rgbtRed = image[n][i].rgbtGreen;
                 image[n][i].rgbtGreen = image[n][i].rgbtBlue;
                 image[n][i].rgbtBlue = image[n][i].rgbtRed;
