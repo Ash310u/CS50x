7b19ef6a3ec6482c19390b6632ba1d635f5d64f6
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-08-30 22:49:14 +0000

    Sat, Aug 31, 2024, 4:19 AM +05:30

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -91,22 +91,12 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     }
     return;
 }
-{
-    // Create a copy of image
-
-    RGBTRIPLE copy[height][width];
-    for (int i = 0; i < height; i++)
-    {
-        for (int j = 0; j < width; j++)
-        {
-            copy[i][j] = image[i][j];
-        }
-    }
-}
 
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE copy[height][width];
+
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
