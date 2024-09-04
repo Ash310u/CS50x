17f9085430a3fc59ef08bb6136ea9570cc70ccdf
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-09-04 04:20:32 +0000

    Wed, Sep 4, 2024, 9:50 AM +05:30

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -85,7 +85,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     }
 }
 
-Detect edges
+// Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     RGBTRIPLE temp[height][width];
