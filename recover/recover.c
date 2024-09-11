ba398496ba2aab1e81b983213a982f07726b8761
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-09-11 14:33:08 +0000

    Wed, Sep 11, 2024, 8:03 PM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
     // Accept a single command-line argument
 
     // Open the memory card
-
+    FILE *f = fopen(,"r");
     // While there's still data left to read from the memory card
 
         // Create JPEGs from the data
