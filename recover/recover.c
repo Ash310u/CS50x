ce55c9157b610e36e9170e2ac93a42400ec5b0d9
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-09-13 04:30:02 +0000

    Fri, Sep 13, 2024, 10:00 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -24,9 +24,10 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             char *filename;
-            sprintf(filename, "%03i.jpg", 2);
+            sprintf(filename, "%03i.jpg", filefound);
             filefound++
-            fwrite(&buffer, sizeof(buffer), 512, filename)
+            FILE *img = fopen(filename, "w")
+            fwrite(&buffer, sizeof(buffer), 512, img);
         }
         return 0;
 
