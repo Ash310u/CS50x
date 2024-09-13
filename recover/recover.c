e88db677401352de79be176a737d36a2f94db563
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-09-13 04:43:21 +0000

    Fri, Sep 13, 2024, 10:13 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -23,9 +23,11 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            // Create JPEGs from the data
             char filename[8];
             sprintf(filename, "%03i.jpg", filefound);
             filefound++;
+
             FILE *img = fopen(filename, "w");
             fwrite(&buffer, sizeof(buffer), 512, img);
         }
@@ -33,5 +35,4 @@ int main(int argc, char *argv[])
 
     }
 
-    // Create JPEGs from the data
 }
