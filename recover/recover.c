3234e2ff54d27e5d9e8ec156fec29073799cbda4
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-09-13 05:21:24 +0000

    Fri, Sep 13, 2024, 10:51 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     FILE *outptr = NULL;
 
     // While there's still data left to read from the memory card
-    while (fread(buffer, 1, 512, card) == 512)
+    while (fread(buffer, 512, 1, card) == 1)
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
             (buffer[3] & 0xf0) == 0xe0)
@@ -53,7 +53,7 @@ int main(int argc, char *argv[])
             }
             file_count++;
         }
-        fwrite(&buffer, sizeof(buffer), 512, img);
-        return 0;
     }
+    
+        fwrite(&buffer, sizeof(buffer), 512, img);
 }
