a3fee0a6a0bcc61e20d1e2eebefe1a3d3352b7c0
Ashu <107404699+Ash310u@users.noreply.github.com>
2024-09-13 05:22:45 +0000

    Fri, Sep 13, 2024, 10:52 AM +05:30

--- recover/recover.c
+++ recover/recover.c
@@ -31,8 +31,7 @@ int main(int argc, char *argv[])
     // While there's still data left to read from the memory card
     while (fread(buffer, 512, 1, card) == 1)
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff &&
-            (buffer[3] & 0xf0) == 0xe0)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             if (found_JPEG)
             {
@@ -54,6 +53,12 @@ int main(int argc, char *argv[])
             file_count++;
         }
     }
-    
-        fwrite(&buffer, sizeof(buffer), 512, img);
+    if(found_JPEG)
+    {
+        while (fread(buffer, 1, 512, card) == 512)
+        {
+            fwrite(&buffer, sizeof(buffer), 512, img);
+
+        }
+    }
 }
